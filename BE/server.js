980e473ce85c8f597909518c0fc11f26f6869ade
Levan_Kurkhuli <levan_kurkhuli@epam.com>
2023-03-03 23:36:04 +0200

    Server initial commit

---
+++ BE/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const pathToStatic = path.resolve(__dirname, '../FE/dist/web-application-of-the-curriculum-management-system/');
+
+app.use(express.static(pathToStatic));
+
+app.get('/*', (req, res) => res.sendFile(path.resolve(pathToStatic, './index.html')));
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`App running on: http://localhost:${port}`));
