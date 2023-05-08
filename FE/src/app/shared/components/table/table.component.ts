b5d23d9f47eef8dbca3242412fe3a6269a73c20d
Levan_Kurkhuli <levan_kurkhuli@epam.com>
2023-05-08 15:53:46 +0300

    reactive forms instead of template form

--- FE/src/app/shared/components/table/table.component.ts
+++ FE/src/app/shared/components/table/table.component.ts
@@ -4,6 +4,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { EducationPlanForTerm } from '../../models/education-plan';
 import { educationPlan, tableColumns } from './table-data';
 import { animate, state, style, transition, trigger } from '@angular/animations';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'lk-table',
@@ -27,37 +28,28 @@ export class TableComponent {
   columnsToDisplayWithExpand = [...this.displayedColumns.map(col => col.key), 'expand'];
   expandedElement: any;
 
-  exam: any;
-  RGR: any;
-  lectures1: any;
-  lectures2: any;
-  practical1: any;
-  practical2: any;
-  credits: any;
+
+  form: FormGroup = this.fb.group({
+    exam: this.fb.control(null),
+    RGR: this.fb.control(null),
+    credits: this.fb.control(null),
+    lectures: this.fb.group({
+      lectures1: this.fb.control(null),
+      lectures2: this.fb.control(null),
+    }),
+    practical: this.fb.group({
+      practical1: this.fb.control(null),
+      practical2: this.fb.control(null),
+    }),
+  });
+
+  get lecturesFormGroup() {
+    return this.form.get('lectures') as FormGroup;
+  }
+
+  get practicalFormGroup() {
+    return this.form.get('practical') as FormGroup;
+  }
+
+  constructor(private fb: FormBuilder) {}
 }
-
-interface Transaction {
-  item: string;
-  cost: number;
-}
-
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
