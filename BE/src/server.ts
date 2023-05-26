a549dd35ddfdd65ee439684b9ca274f019a1631b
Levan_Kurkhuli <levan_kurkhuli@epam.com>
2023-05-26 12:20:01 +0300

    removed static

--- BE/src/server.ts
+++ BE/src/server.ts
@@ -4,22 +4,13 @@ import path from "path";
 import http from 'http';
 import authRouter from "./api/login";
 import epRouter from "./api/education-plan";
-import { fileURLToPath } from 'url';
 import healthxRouter from './api/healthz';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-const pathToStatic = path.resolve(__dirname, '../../FE/dist/web-application-of-the-curriculum-management-system/');
-
-app.use(express.static(pathToStatic));
 app.use(express.json())
-
-app.get('/', (req: Request, res: Response) => res.sendFile(path.resolve(pathToStatic, './index.html')));
 
 app.use('/healthz', healthxRouter);
 
